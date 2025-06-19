56f72e01e78a12e5691039f0d9e6aeeb4ee8e403
Arindam Kumar <kumararindamAK.2002@gmail.com>
2025-06-19 13:10:13 +0530

    Initial frontend commit

---
+++ src/assets/assets.js
@@ -0,0 +1,132 @@
+import logo from './logo.svg'
+import PromptArt from './PromptArt.svg'
+import PromptArt_logo from './PromptArt_logo.svg'
+import PromptArt_logo_top from './PromptArt_logo_top.svg'
+import logo1 from './repair-svgrepo-com.svg'
+import logo2 from './file.svg'
+import logo_icon1 from './logo_icon1.png'
+import logo_icon from './logo_icon.svg'
+import facebook_icon from './facebook_icon.svg'
+import instagram_icon from './instagram_icon.svg'
+import twitter_icon from './twitter_icon.svg'
+import star_icon from './star_icon.svg'
+import rating_star from './rating_star.svg'
+import sample_img_1 from './sample_img_1.png'
+import sample_img_2 from './sample_img_2.png'
+import sample_img_3 from './sample_img_3.png'
+import sample_img_4 from './sample_img_4.png'
+import sample_img_5 from './sample_img_5.png'
+import sample_img_6 from './sample_img_6.png'
+import sample_img_7 from './sample_img_7.png'
+import sample_img_8 from './sample_img_8.png'
+import sample_img_9 from './sample_img_9.png'
+import sample_img_10 from './sample_img_10.png'
+import sample_img_11 from './sample_img_11.png'
+import profile_img_1 from './profile_img_1.png'
+import profile_img_2 from './profile_img_2.png'
+import profile_img_3 from './profile_img_3.png'
+import step_icon_1 from './step_icon_1.svg'
+import step_icon_2 from './step_icon_2.svg'
+import step_icon_3 from './step_icon_3.svg'
+import email_icon from './email_icon.svg'
+import lock_icon from './lock_icon.svg'
+import cross_icon from './cross_icon.svg'
+import star_group from './star_group.png'
+import credit_star from './credit_star.svg'
+import profile_icon from './profile_icon.png'
+
+export const assets = {
+    logo,
+    PromptArt,
+    PromptArt_logo,
+    PromptArt_logo_top,
+    logo1,
+    logo2,
+    logo_icon,
+    logo_icon1,
+    facebook_icon,
+    instagram_icon,
+    twitter_icon,
+    star_icon,
+    rating_star,
+    sample_img_1,
+    sample_img_2,
+    sample_img_3,
+    sample_img_4,
+    sample_img_5,
+    sample_img_6,
+    sample_img_7,
+    sample_img_8,
+    sample_img_9,
+    sample_img_10,
+    sample_img_11,
+    email_icon,
+    lock_icon,
+    cross_icon,
+    star_group,
+    credit_star,
+    profile_icon
+}
+
+export const stepsData = [
+    {
+      title: 'Describe Your Vision',
+      description: 'Type a phrase, sentence, or paragraph that describes the image you want to create.',
+      icon: step_icon_1,
+    },
+    {
+      title: 'Watch the Magic',
+      description: 'Our AI-powered engine will transform your text into a high-quality, unique image in seconds.',
+      icon: step_icon_2,
+    },
+    {
+      title: 'Download & Share',
+      description: 'Instantly download your creation or share it with the world directly from our platform.',
+      icon: step_icon_3,
+    },
+  ];
+
+export const testimonialsData = [
+    {
+        image:profile_img_1,
+        name:'Donald Jackman',
+        role:'Graphic Designer',
+        stars:5,
+        text:`PromptArt is my secret weapon for prototyping hero sections and concept art. The image quality is impressive, and it really helps clients visualize early ideas.`
+    },
+    {
+        image:profile_img_2,
+        name:'Richard Nelson',
+        role:'Content Creator',
+        stars:4,
+        text:`I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
+    },
+    {
+        image:profile_img_3,
+        name:'Gary Vaynerchuk',
+        role:' Digital Marketer',
+        stars:5,
+        text:`From ad banners to blog images, PromptArt helps me create creative content instantly. I love how easy it is to turn text into high-quality visuals.`
+    },
+]
+
+export const plans = [
+    {
+      id: 'Basic',
+      price: 4411.76,
+      credits: 100,
+      desc: 'Best for personal use.'
+    },
+    {
+      id: 'Advanced',
+      price: 28235.29,
+      credits: 1000,
+      desc: 'Best for business use.'
+    },
+    {
+      id: 'Business',
+      price: 70588.23,
+      credits: 5000,
+      desc: 'Best for enterprise use.'
+    },
+  ]
