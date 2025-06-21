86e29285f964492fd909954b63b190924c1894e4
Arindam Kumar <kumararindamAK.2002@gmail.com>
2025-06-21 15:13:04 +0530

    Navbar.jsx updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,16 +10,48 @@ export default function Navbar()
 	const navigate=useNavigate()
 
 	return (
-		<div className='flex items-center justify-between py-4' >
+		<div className='flex items-center justify-between' >
 			<Link to='/'>
 			<div className='flex flex-row w-28 sm:w-32 lg:w-40'>
 				<img src={assets.PromptArt_logo} alt="" />
 			</div>
 			</Link>
 
+			{/*<div className='flex items-center gap-2 sm:gap-5'>
+				<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/terms' className='cursor-pointer'>
+					Terms & Conditions
+				</a>
+				<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/shipping' className='cursor-pointer'>
+					Shipping & Delivery
+				</a>
+				<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/refund' className='cursor-pointer'>
+					Cancellation & Refund
+				</a>
+				<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/privacy' className='cursor-pointer'>
+					Privacy
+				</a>
+				<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/contact_us' className='cursor-pointer'>
+					Contact Us
+				</a>
+			</div>*/}
 			<div>
 				{user ?
 					<div className='flex items-center gap-2 sm:gap-3'>
+						<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/terms' className='cursor-pointer text-gray-600 max-sm:hidden pl-4'>
+							Terms
+						</a>
+						<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/shipping' className='cursor-pointer text-gray-600 max-sm:hidden pl-4'>
+							Shipping
+						</a>
+						<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/refund' className='cursor-pointer text-gray-600 max-sm:hidden pl-4'>
+							Refund
+						</a>
+						<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/privacy' className='cursor-pointer text-gray-600 max-sm:hidden pl-4'>
+							Privacy
+						</a>
+						<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/contact_us' className='cursor-pointer text-gray-600 max-sm:hidden pl-4'>
+							Contact Us
+						</a>
 						<button onClick={()=>navigate('/buy')} className='flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700'>
 							<img className='w-5' src={assets.credit_star} alt="" />
 							<p className='text-xs sm:text-sm font-medium text-gray-600'>
@@ -42,6 +74,21 @@ export default function Navbar()
 					</div>
 				:
 					<div className='flex items-center gap-2 sm:gap-5'>
+						<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/terms' className='cursor-pointer'>
+							Terms & Conditions
+						</a>
+						<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/shipping' className='cursor-pointer'>
+							Shipping & Delivery
+						</a>
+						<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/refund' className='cursor-pointer'>
+							Cancellation & Refund
+						</a>
+						<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/privacy' className='cursor-pointer'>
+							Privacy
+						</a>
+						<a href='https://merchant.razorpay.com/policy/QgoNdqOm2fuFll/contact_us' className='cursor-pointer'>
+							Contact Us
+						</a>
 						<p onClick={()=>navigate('/buy')} className='cursor-pointer'>
 							Pricing
 						</p>
